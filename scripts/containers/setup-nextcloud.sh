19ada117a5a3533a5ce9cc4c465c430d4a9edb45
Luca Di Bello <lucadibello@proton.me>
2025-02-24 11:08:20 +0100

    Removed unused config script

--- scripts/containers/setup-nextcloud.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create nextcloud volume
-sudo docker volume create nextcloud_aio_mastercontainer || true
-
-# Create network for nextcloud
-sudo docker network create nextcloud-aio || true
