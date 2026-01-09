f602fcef25a386987a0571fd7c40cdb5a6fd9d02
Luca Di Bello <info@lucadibello.ch>
2026-01-09 18:26:00 +0100

    fix: now assigning correct UID/GID to avoid permission errors

--- scripts/containers/setup-loki.sh
+++ scripts/containers/setup-loki.sh
@@ -2,3 +2,8 @@
 
 # Create loki network
 sudo docker network create loki-network || true
+
+# Fix permissions for Loki (runs as 10001)
+if [ -d "./composes/loki" ]; then
+    chown -R 10001:10001 ./composes/loki
+fi
