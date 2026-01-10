a8f3ca7956b1cf8f98b2ddcae6adb1bd1ce166e2
Luca Di Bello <info@lucadibello.ch>
2026-01-10 14:58:41 +0100

    fix: now using correct permissions

--- scripts/os/setup-os.sh
+++ scripts/os/setup-os.sh
@@ -40,6 +40,7 @@ fi
 # Add Docker repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod 644 /etc/apt/sources.list.d/docker.list
 
 # Update and install Docker Engine with Compose plugin
 echo "[*] Installing Docker Engine and Compose plugin..."
